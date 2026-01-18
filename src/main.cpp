0698a43fe7630a272af8952645daecf98b7a4e14
Nadish-prog <nadishethereum@gmail.com>
2026-01-19 01:19:42 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,17 @@ int positive_char(const std::string& input_line,const std::string& pattern ){
     return 0;
 }
 
+int negative_char(const std::string& input_line,const std::string& pattern ){
+    for(char c: pattern){
+        for(char i : input_line){
+            if(i == c){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
@@ -35,9 +46,16 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
         return false;
     }
     }
-    if((pattern.front() == '[') && (pattern.back() == ']') ){
+    
+    if((pattern.front() == '[') && (pattern.back() == ']') )
+    {
+        if(pattern[1]=='^'){
+                return negative_char(input_line, pattern.substr(2,pattern.length() - 3) ); 
+        }
+        else
           return positive_char(input_line, pattern.substr(1,pattern.length() - 2) ); 
     }
+
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
