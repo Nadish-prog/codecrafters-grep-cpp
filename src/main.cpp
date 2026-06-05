2edaa87b4a1e7bb091e4a55b1c87e59389d5d607
Nadish-prog <nadishethereum@gmail.com>
2026-06-06 01:23:59 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-int positive_char_group(const std::string& input_line,const std::string& pattern ){
+std::vector<std::string> parsepattern(std::string p){
+    std::vector<std::string>arr;
+    for(int i =0;i<p.length();i++){
+        if(p[i] == '\\'){
+            arr.push_back(std::string("\\")+p[i+1]);
+            i++;
+        }
+       else  if(p[i] == '['){
+           std::string s = "";
+            while(p[i]!=']'){
+                s=s+p[i];
+                i++;
+            }
+            s+=']';
+            arr.push_back(s);
+        }
+        else{
+            arr.push_back(std::string(1,p[i]));
+        }
+    }
+    return arr;
+}
+
+
+bool positive_char_group(const std::string& input_line,const std::string& pattern ){
     for(char c: pattern){
         for(char i : input_line){
             if(i == c){
-                return 1;
+                return true;
             }
         }
     }
-    return 0;
+    return false;
 }
 
-int negative_char_group(const std::string& input_line,const std::string& pattern ){
+bool negative_char_group(const std::string& input_line,const std::string& pattern ){
     for(char c: input_line){
         int count =0;
         for(char i : pattern){
@@ -21,48 +46,98 @@ int negative_char_group(const std::string& input_line,const std::string& pattern
             }
         }
         if(count==0){
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
 }
 
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    if (pattern.length() == 1) {
-        return input_line.find(pattern) != std::string::npos;
-    }
-    if(pattern.length() == 2)
-    {
-      if(pattern =="\\d" ){
-        for(char c : input_line){
+bool digit_matcher(const std::string& input_line, const std::string& pattern){
+ for(char c : input_line){
             if(c>='0' && c<='9'){
                 return true;
             }
         }
         return false;
+}
+
+
+bool firstRule(char s,std::string &pattern){
+  
+   
+     if(pattern == "\\d"){
+       
+       if((s>='0' && s<='9'))
+            {
+                return true;
+            }
     }
-    if(pattern == "\\w"){
+    else if(pattern == "\\w"){
+         if((s>='0' && s<='9') || (s>='a' && s<='z') || (s>='A' && s<='z')|| (s == '_'))
+            {
+                return true;
+            }
+    }
+    else if(pattern.front() == '[' && pattern.back()==']'){
+        if(pattern[1] == '^'){
+            return negative_char_group(std::string(1, s), pattern.substr(2, pattern.length() - 3));
+        }
+        else{
+            return positive_char_group(std::string(1, s), pattern.substr(1, pattern.length() - 2));
+        }
+    }
+    else {
+        if(s==pattern[0]){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool word_char_matcher(const std::string& input_line, const std::string& pattern){
         for(char c:input_line){
             if((c>='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='z')|| (c == '_')){
                 return true;
             }
         }
         return false;
-    }
-    }
-    
-    if((pattern.front() == '[') && (pattern.back() == ']') )
-    {
-        if(pattern[1]=='^'){
-                return negative_char_group(input_line, pattern.substr(2,pattern.length() - 3) ); 
-        }
-        else
-          return positive_char_group(input_line, pattern.substr(1,pattern.length() - 2) ); 
-    }
+}
 
-    else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
+bool patternChecker(std::string s, int j, std::vector<std::string>&pattern){
+    int k = j;
+    for(int i = 0;i<pattern.size();i++){
+        
+        if(firstRule(s[k],pattern[i])){
+            k++;
+        }
+        else{
+          
+            return false;
+        }
     }
+    return true;
+}
+
+
+bool match_pattern(const std::string& input_line, const std::string& pattern) {
+    std::vector<std::string> arr = parsepattern(pattern);
+   for(int i = 0;i<input_line.size();i++){
+     
+      if(firstRule(input_line[i],arr[0])){
+          if(patternChecker(input_line,i,arr)){
+             
+              return true;
+              
+          }
+      }
+  }
+    
+    return false;
+
+    // else {
+    //     throw std::runtime_error("Unhandled pattern " + pattern);
+    // }
 }
 
 int main(int argc, char* argv[]) {
@@ -86,8 +161,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // TODO: Uncomment the code below to pass the first stage
-    //
+    
     std::string input_line;
     std::getline(std::cin, input_line);
     
